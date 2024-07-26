a564e452e6063ad260db03d0f5f2dcad731171c6
Zach Johnson <zachjohnson@Zachs-MBP-2.attlocal.net>
2024-07-26 15:03:37 -0500

    added nft website demo

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,7 +4,7 @@ import{BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 
 import {Link} from 'react-router-dom'
 import iphones_image from '../../assets/phones.png'
-import smoothie_image from '../../assets/smoothie-ph.jpg'
+import nft_image from '../../assets/nft.png'
 import game_image from '../../assets/gameco-ss.png'
 import { FaExternalLinkAlt } from "react-icons/fa";
 import github_icon from '../../assets/github-mark.png'
@@ -68,35 +68,35 @@ const Projects = () => {
 
       </div>
 
-      {/* <div className='project'>
+      <div className='project'>
 
 
 
       <div className='project__info__mid'>
-        <h3>smoothie shop</h3>
+      
+        <h3>Nft Wallet Viewer</h3>
 
-        <p>A website for a game company. Browse their library and download their games.</p>
+        <p>A website that makes it easy to view nft collections. 
+        Simply plug in a wallet's public address and the user will be able to tab through each nft in the collection.
+        The site also provides celebrity nft wallets the user can check out if they get curious.
+        </p>
 
-        <div className='project__tech'>
-          <h2 className='tech__left'>React</h2>
-          <h2 className='tech__right'>Vanilla Css</h2>
-        </div>
         
         <div className='project__links'>
         <Router>
 
           <div className='github_link'>
-            <Link to='https://github.com/Zach-Yates' className='code_text'>Code</Link>
+            <Link to='https://github.com/Zach-Yates/portfolio-nft-wallet-viewer' className='code_text'>Code</Link>
 
-            <Link to='https://github.com/Zach-Yates'>
+            <Link to='https://github.com/Zach-Yates/portfolio-nft-wallet-viewer'>
               <img className='icon' src={github_icon} />
             </Link>
           </div>
 
           <div className='demo_link'>
-            <Link to='/gameHome' className='demo_text'>Live Demo</Link>
+            <Link to='https://zach-yates.github.io/portfolio-nft-wallet-viewer/' className='demo_text' onClick={() => window.open('https://zach-yates.github.io/portfolio-computer-company/','_self')}>Live Demo</Link>
 
-            <Link to='/gameHome'>
+            <Link to='https://zach-yates.github.io/portfolio-nft-wallet-viewer/' onClick={() => window.open('https://zach-yates.github.io/portfolio-computer-company/','_self')}>
               <FaExternalLinkAlt size="20px" color="black"/>
             </Link>
           </div>
@@ -105,10 +105,10 @@ const Projects = () => {
       </div>
 
       <div className='project__img__mid'>
-        <img src={smoothie_image}/>
+        <img src={nft_image}/>
       </div>
 
-      </div> */}
+      </div>
 
 
 
