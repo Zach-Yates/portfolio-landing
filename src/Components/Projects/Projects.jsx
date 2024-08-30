228547a7db9c78029131873253ec3fb99b8c5fc5
Zach Johnson <zachjohnson@Zachs-MBP-2.attlocal.net>
2024-08-30 14:28:17 -0500

    added lore app

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,6 +6,7 @@ import {Link} from 'react-router-dom'
 import iphones_image from '../../assets/phones.png'
 import nft_image from '../../assets/nft.png'
 import game_image from '../../assets/gameco-ss.png'
+import lore_image from '../../assets/lore-ss.png'
 import { FaExternalLinkAlt } from "react-icons/fa";
 import github_icon from '../../assets/github-mark.png'
 
@@ -23,6 +24,7 @@ const Projects = () => {
         <h2>Dynamic design meets seamless functionality.</h2>
       </div>
 
+      {/* PROJECT */}
       <div className='project'>
 
         <div className='project__img'>
@@ -68,50 +70,48 @@ const Projects = () => {
 
       </div>
 
+      {/* PROJECT */}
       <div className='project'>
 
+<div className='project__info__mid'>
 
+  <h3>More Lore!</h3>
 
-      <div className='project__info__mid'>
+  <p>A web app that makes it easy for users learn about their favorite game world.
+  Each game has a collection of videos including a story recap, speedrun, full playthrough, and retrospective.
+  Users can even create an account to track their progress!
+  </p>
+
+  
+  <div className='project__links'>
+  <Router>
+
+    {/* <div className='github_link'>
+      <Link to='https://github.com/Zach-Yates/portfolio-nft-wallet-viewer' className='code_text'>Code</Link>
+
+      <Link to='https://github.com/Zach-Yates/portfolio-nft-wallet-viewer'>
+        <img className='icon' src={github_icon} />
+      </Link>
+    </div> */}
+
+    <div className='demo_link'>
+      <Link to='https://lore-199e6.web.app/' className='demo_text' onClick={() => window.open('https://lore-199e6.web.app/','_self')}>Live Demo</Link>
+
+      <Link to='https://lore-199e6.web.app/' onClick={() => window.open('https://lore-199e6.web.app/','_self')}>
+        <FaExternalLinkAlt size="20px" color="black"/>
+      </Link>
+    </div>
+    </Router>
+  </div>
+</div>
+
+<div className='project__img__mid'>
+  <img src={lore_image}/>
+</div>
+
+      </div>
       
-        <h3>Nft Wallet Viewer</h3>
-
-        <p>A website that makes it easy to view nft collections. 
-        Simply plug in a wallet's public address and the user will be able to tab through each nft in the collection.
-        The site also provides celebrity nft wallets the user can check out if they get curious.
-        </p>
-
-        
-        <div className='project__links'>
-        <Router>
-
-          <div className='github_link'>
-            <Link to='https://github.com/Zach-Yates/portfolio-nft-wallet-viewer' className='code_text'>Code</Link>
-
-            <Link to='https://github.com/Zach-Yates/portfolio-nft-wallet-viewer'>
-              <img className='icon' src={github_icon} />
-            </Link>
-          </div>
-
-          <div className='demo_link'>
-            <Link to='https://zach-yates.github.io/portfolio-nft-wallet-viewer/' className='demo_text' onClick={() => window.open('https://zach-yates.github.io/portfolio-computer-company/','_self')}>Live Demo</Link>
-
-            <Link to='https://zach-yates.github.io/portfolio-nft-wallet-viewer/' onClick={() => window.open('https://zach-yates.github.io/portfolio-computer-company/','_self')}>
-              <FaExternalLinkAlt size="20px" color="black"/>
-            </Link>
-          </div>
-          </Router>
-        </div>
-      </div>
-
-      <div className='project__img__mid'>
-        <img src={nft_image}/>
-      </div>
-
-      </div>
-
-
-
+      {/* PROJECT */}
       <div className='project'>
 
       <div className='project__img'>
@@ -148,6 +148,49 @@ const Projects = () => {
           </div>
           </Router>
         </div>
+      </div>
+
+      </div>
+
+     {/* PROJECT */}
+     <div className='project'>
+
+
+
+      <div className='project__info__mid'>
+
+        <h3>Nft Wallet Viewer</h3>
+
+        <p>A website that makes it easy to view nft collections. 
+        Simply plug in a wallet's public address and the user will be able to tab through each nft in the collection.
+        The site also provides celebrity nft wallets the user can check out if they get curious.
+        </p>
+
+        
+        <div className='project__links'>
+        <Router>
+
+          <div className='github_link'>
+            <Link to='https://github.com/Zach-Yates/portfolio-nft-wallet-viewer' className='code_text'>Code</Link>
+
+            <Link to='https://github.com/Zach-Yates/portfolio-nft-wallet-viewer'>
+              <img className='icon' src={github_icon} />
+            </Link>
+          </div>
+
+          <div className='demo_link'>
+            <Link to='https://zach-yates.github.io/portfolio-nft-wallet-viewer/' className='demo_text' onClick={() => window.open('https://zach-yates.github.io/portfolio-computer-company/','_self')}>Live Demo</Link>
+
+            <Link to='https://zach-yates.github.io/portfolio-nft-wallet-viewer/' onClick={() => window.open('https://zach-yates.github.io/portfolio-computer-company/','_self')}>
+              <FaExternalLinkAlt size="20px" color="black"/>
+            </Link>
+          </div>
+          </Router>
+        </div>
+      </div>
+
+      <div className='project__img__mid'>
+        <img src={nft_image}/>
       </div>
 
       </div>
